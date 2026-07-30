38a4c073cfa85c2a091c86460ee19ccb388d187a
julia-mfer <julia.mferreira@aluno.edu.es.gov.br>
2026-07-30 13:39:03 -0300

    Update alerta_faltas.sql

--- alerta_faltas.sql
+++ alerta_faltas.sql
@@ -1,10 +1,1 @@
-CREATE DATABASE alerta_faltas;
-USE alerta_faltas; 
 
-CREATE TABLE relatorio_temp(
- num_linha int auto_increment primary key,
- aluno varchar(200),
- id_aluno int,
- total_faltas int,
- status varchar(20)
-);
