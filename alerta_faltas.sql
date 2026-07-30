a68f0b7c653fd08cea95fe6c5543035e16c30fb2
julia-mfer <julia.mferreira@aluno.edu.es.gov.br>
2026-07-30 13:42:48 -0300

    Add files via upload

--- alerta_faltas.sql
+++ alerta_faltas.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE alerta_faltas; 
+USE alerta_faltas; 
 
+CREATE TABLE relatorio_temp(
+ num_linha int auto_increment primary key, 
+ id_aluno int,
+ aluno varchar(200),
+ turma varchar(20),
+ total_faltas int,
+ status varchar(50)
+);
+
+CREATE TABLE tab_config(
+id_configuracao int auto_increment primary key, 
+limite int not null
+);
+
+INSERT INTO tab_config(limite) values(100);
