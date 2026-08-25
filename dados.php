1cb81328d1d49346330e97d599a4756f90f1c886
sopinha-hash <sophia.faria@aluno.edu.es.gov.br>
2026-08-25 08:38:41 -0300

    Update dados.php

--- dados.php
+++ dados.php
@@ -214,23 +214,25 @@ $alunos_filtrados = array_filter($alunos, function($aluno) use ($turma_seleciona
         </button>
       </div>
 
-      <button class="btn-primary" id="btn-enviar-email">
-        <div class="btn-icon">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-            <polyline points="22,6 12,13 2,6"></polyline>
-          </svg>
-        </div>
-        <div class="btn-text">
-          <span>ENVIAR E-MAIL DE ALERTA</span>
-          <small>Gerar arquivo com alunos selecionados</small>
-        </div>
-      </button>
+<button type="button" class="btn-primary" id="btn-enviar-email" onclick="abrirModalEmail()">
+  <div class="btn-icon">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+      <polyline points="22,6 12,13 2,6"></polyline>
+    </svg>
+  </div>
+  <div class="btn-text">
+    <span>ENVIAR E-MAIL DE ALERTA</span>
+    <small>Gerar arquivo com alunos selecionados</small>
+  </div>
+</button>
     </div>
   </div>
 
 </main>
 
 <script src="dados.js"></script>
+<?php include 'alerta.php'; ?>
+</div>
 </body>
 </html>
